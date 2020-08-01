5cceca24d9ed0525df0a88d8ac23004f95231785
Raul Pereira <raul.pereira@gmail.com>
2020-07-31 21:00:23 -0300

    class 05 - fix heroku config

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const BACKEND_BASE_URL = window.location.href.includes('localhost')
   ? 'http://localhost:8080'
-  : 'http://blablabla.herokuapp.com';
+  : 'https://rp7flix.herokuapp.com';
 
 export default {
   BACKEND_BASE_URL,
